66abb5cb14e374eb0c0f27b7acda7f8db089f00d
Kiran Asmat <asmatkiran9@gmail.com>
2025-09-06 01:21:57 +0500

    databaseConnection

---
+++ app/lib/dbConnected.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+let isConnected = false;
+export default async function connectDB() {
+    if (isConnected) {
+        console.log("mongoDB is already connected")
+        return
+    }
+    try {
+        await mongoose.connect(process.env.MONGODB_URI) 
+        isConnected = true;
+        console.log("MongoDB connected succesfully")
+    } catch (error) {
+        console.error("MongoDB connection error",error)
+    }
+}
